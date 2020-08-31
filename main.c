b459a107c81f73477c74785663c74d8e77b28338
H4ckerxx44 <dennis.wortmann1999@gmail.com>
2020-08-31 23:33:41 +0200

    -- updated .gitignore

--- main.c
+++ main.c
@@ -10,7 +10,6 @@ double main()
 	double item_array[4] = {19.99, 24.99, 4.99, 99.99};
 	for (int n = 0; n < 4; n++)
 		total = total + item_array[n];
-		
 	double tax = (total / 100) * vat;
 	double subtotal = total - tax;
 	printf("\n\n");
